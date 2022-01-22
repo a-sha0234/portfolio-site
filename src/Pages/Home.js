4a0f821c8183e1decad0d16b5a843a1b95543b21
a-sha0234 <asharma_31@hotmail.com>
2022-01-22 20:01:28 +0000

    add styling to the home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,18 +1,27 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Arun Sharma</h1>
-      <h2>Why I enjoy front end development </h2>
-      <p>f</p>
-      <h2> Skill set</h2>
-      <p>Html5</p>
-      <p>css3</p>
-      <p>sass</p>
-      <p>Javascript</p>
-      <p>React js</p>
-      <p> git version control </p>
-      <p> npm </p>
-      <p> webpack</p>
+    <div className="wrapper">
+      <main className="information">
+        <section className="information__self">
+          <h2>Why front end development </h2>
+          <p>f</p>
+        </section>
+        <section className="information__skills">
+          <h2> Skill set</h2>
+          <div className="information__skills__right">
+            <p>Html5</p>
+            <p>css3</p>
+            <p>sass</p>
+            <p>Javascript</p>
+          </div>
+          <div className="information__skills__left">
+            <p>React js</p>
+            <p> git version control </p>
+            <p> npm </p>
+            <p> webpack</p>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
